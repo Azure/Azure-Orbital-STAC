5b8d872cf648b9737aae334a3739acdcd490f367
Kungumaraj Nachimuthu <kunachim@microsoft.com>
2022-12-13 09:46:17 -0800

    Copyrights header added

--- src/azure-stac-cli/stac/metrics/__metrics_factory.py
+++ src/azure-stac-cli/stac/metrics/__metrics_factory.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from stac.metrics.data_metrics import DataMetrics
 from stac.metrics.message_metrics import MessageMetrics
 from stac.metrics.pod_metrics import PodMetrics
